942e8d6f331652d8f4ff66f48fb4c2723f48c3be
dheesh-m <dheeshm@gmail.com>
2024-11-03 14:51:44 +0530

    txt to spech.py
    simultaneously continues the function

--- txt to spech.py
+++ txt to spech.py
@@ -1,9 +1,21 @@
 import pyttsx3
 
+
 txt_speech = pyttsx3.init()
 
-ans  = input("txt conversion:")
 
-txt_speech.say(ans)
+txt_speech.setProperty('rate', 150)  
+txt_speech.setProperty('volume', 1)  
 
-txt_speech.runAndWait()
+while True:
+   
+    ans = input("Enter text for conversion to speech (or type 'exit' to quit): ")
+
+    if ans.lower() == 'exit':
+        print("Exiting the program.")
+        break
+
+ 
+    txt_speech.say(ans)
+
+    txt_speech.runAndWait()
